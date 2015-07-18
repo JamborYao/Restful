99beaa77fd25f3e2246975243898266dab8795f7
jambor <akingyao@126.com>
2015-07-18 08:07:20 +0800

    create list container class

--- Restful/Program.cs
+++ Restful/Program.cs
@@ -15,7 +15,7 @@ namespace Restful
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri("https://danielfiletest.blob.core.windows.net/mycontainer?restype=container&comp=list"));
             request.Method = "GET";
-            request.Headers.Add("x-ms-version", "2012-02-12");
+            request.Headers.Add("x-ms-version", "2014-02-14");
             /*
              身份验证大体分为：基于共享密钥的身份验证、基于生物学特征的身份验证和基于公开密钥加密算法的身份验证。
              基于共享密钥的身份验证是指服务器端和用户共同拥有一个或一组密码
@@ -36,7 +36,7 @@ namespace Restful
             String canonicalizedHeaders = String.Format(
             "x-ms-date:{0}\nx-ms-version:{1}",
             dateString,
-            "2012-02-12");
+            "2014-02-14");
             String canonicalizedResource = "/danielfiletest/mycontainer\nrestype:container\ncomp:list";
 
             String stringToSign = String.Format(
@@ -54,7 +54,9 @@ namespace Restful
             "\n" +
             "{1}\n" +
             "{2}",
-            "GET", canonicalizedHeaders, "/danielfiletest/mycontainer\ncomp:list\nrestype:container"
+            "GET", canonicalizedHeaders, 
+            "/danielfiletest/mycontainer\ncomp:list\nrestype:container"
+            //canonicalizedResource
             );
 
 
---
+++ Restful/listContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace Restful
+{
+    public  class listContainer
+    {
+        string _accountName;
+        string _accountKey;
+        string _listContainerUrl = "";
+        string _xdate = DateTime.UtcNow.ToString("R");
+        string xversion = "2014-02-14";
+
+        public listContainer(string accountName,string accountKey)
+        {
+            this._accountName = accountName;
+            this._accountKey = accountKey;
+        }
+        public void ListContainer()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(_listContainerUrl));
+            request.Headers.Add("x-ms-date", _xdate);
+            request.Headers.Add("x-ms-version", xversion);
+            request.Headers.Add("Authorization", "");
+        }
+        public string SignToString() 
+        {
+
+            HMACSHA256 hmac = new HMACSHA256(Convert.FromBase64String(_accountKey));
+            string authorization= Convert.ToBase64String( hmac.ComputeHash(Encoding.UTF8.GetBytes("")));
+            return authorization;
+        }
+
+    }
+}
